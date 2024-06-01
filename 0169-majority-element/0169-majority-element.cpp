198cacf960e861f7f9919c9aae73b6e5a188af4f
Aditi Chauhan <116872023+Aditii004@users.noreply.github.com>
2024-06-01 22:00:38 +0530

    Time: 23 ms (21.81%), Space: 27.7 MB (32.15%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -3,9 +3,11 @@ public:
     int majorityElement(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         int n=nums.size();
+        return nums[n/2];
+    }
+        
+        
       
-            return nums[n/2];
-        }
         
         
     
